a09eb1c7e20066e3287394ed9fb1a37c45f2861f
Mounish G <mounishganesh2000@gmail.com>
2023-03-28 11:16:06 +0530

    initial commit

---
+++ src/main/java/com/scrapify/scrapifyuser/ScrapifyUserApplication.java
@@ -0,0 +1,13 @@
+package com.scrapify.scrapifyuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrapifyUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrapifyUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scrapify/scrapifyuser/ScrapifyUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scrapify.scrapifyuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrapifyUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
